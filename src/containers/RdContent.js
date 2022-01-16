6ad419bfdc646577110ef319b4b5fce0b9b05830
ccx1010sky <ccx1010@gmail.com>
2022-01-16 20:34:09 +0000

    fetch() to get the data  and useEffect()

--- src/containers/RdContent.js
+++ src/containers/RdContent.js
@@ -1,20 +1,33 @@
-import React from 'react'
-import ContentList from '../components/ContentList'
-import ContentInput from '../components/ContentInput'
-
+import React, { useEffect, useState } from "react";
+import ContentList from "../components/ContentList";
+import ContentInput from "../components/ContentInput";
 
 const RdContent = () => {
+    
+  const [posts, setPosts] = useState([]);
 
+useEffect(() => {
+    getPosts()      
+}, [])
+    
+  const getPosts = () => {
+      console.log("hello getPosts");
+    const request = fetch("https://www.reddit.com/r/javascript.json")
+    .then(res => res.json())
+    .then(res => res.data)
+    .then(posts => setPosts(posts))
 
+   
+    console.log(request);
+    
+  };
 
-    return (
-        <div>
+  return (
+    <div>
+      <ContentList />
+      <ContentInput />
+    </div>
+  );
+};
 
-            <ContentList />
-            <ContentInput />
-            
-        </div>
-    )
-}
-
-export default RdContent
+export default RdContent;
